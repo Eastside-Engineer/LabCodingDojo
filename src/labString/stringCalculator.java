e1520cf7d1c7c28d35c02a1fd3738978a107c2e2
Cynthia <cturner@ltu.edu>
2019-02-15 09:05:49 -0500

    done

--- src/labString/stringCalculator.java
+++ src/labString/stringCalculator.java
@@ -1,42 +1,78 @@
 package labString;
 
 public class stringCalculator {
-
-	public static String add(String s) {
-		if (s == "") {
-			return "0";
-		}
-		if (s.contains(",") && s.contains(".")) {
-			String[] values = s.split(",");
-			String value1 = values[0];
-			String value2 = values[1];
-			double x = Double.parseDouble(value1);
-			double y = Double.parseDouble(value2);
 		
+		public static String add(String s) {
+			if (s == "") {
+				return "0";
+			}
+			if (s == "0") {
+				return "";
+			}
 
-			double preResult = x + y;
-			String result = Double.toString(preResult);
+			if (s.contains(",") && s.contains(".")) {
 
-			return result;
+				String[] values = s.split(",");
 
+				String value3 = values[0];
+				String value4 = values[1];
+
+				double x = Double.parseDouble(value3);
+				double y = Double.parseDouble(value4);
+
+				double preResult = x + y;
+
+				String result = Double.toString(preResult);
+
+				return result;
+
+			}
+
+			if (s.contains("-") && s.contains(",")) {
+
+				String[] values = s.split(",");
+
+				String value89 = values[0];
+				String value90 = values[1];
+
+				int x = Integer.parseInt(value89);
+				int y = Integer.parseInt(value90);
+
+				int preResult = x + y;
+
+				String result = Integer.toString(preResult);
+
+				return result;
+
+			}
+
+			if (s.contains("-")) {
+
+				String result = s;
+
+				return result;
+
+			}
+
+			if (s.contains(",")) {
+
+				String[] values = s.split(",");
+
+				String value1 = values[0];
+				String value2 = values[1];
+
+				int x = Integer.parseInt(value1);
+				int y = Integer.parseInt(value2);
+
+				int preResult = x + y;
+
+				String result = Integer.toString(preResult);
+
+				return result;
+
+			}
+
+			return s;
 		}
-		if (s.contains(",")) {
-			String[] values = s.split(",");
-			String value1 = values[0];
-			String value2 = values[1];
-
-			int x = Integer.parseInt(value1);
-			int y = Integer.parseInt(value2);
-
-			int preResult = x + y;
-			String result = Integer.toString(preResult);
-
-			return result;
-
-		}
-
-		return s;
-
 	}
-
-}
+	
--- src/labString/stringCalculatorTest.java
+++ src/labString/stringCalculatorTest.java
@@ -1,79 +1,133 @@
 package labString;
 
 import static org.junit.Assert.assertEquals;
-	import org.junit.jupiter.api.Test;
-	
+import org.junit.jupiter.api.Test;
+
 public class stringCalculatorTest {
-	
-	    @Test
+
+	@Test
 	/// TC 7
-	    void StringCalculatorTest() {
-	// arrange
-	        String expected = "7";
-	// actual
-	        String actual = stringCalculator.add("7");
-	// assert
-	        assertEquals(expected, actual);
-	    }
-	    @Test
-	/// TC 0 
-	    void StringCalculatorTest2() {
-	// arrange
-	        String expected = "0";
-	// actual
-	        String actual = stringCalculator.add("0");
-	// assert
-	        assertEquals(expected, actual);
-	    }
-	    @Test
-	    void StringCalculatorTest3() {
-	// arrange
-	        String expected = "100";
-	// actual
-	        String actual = stringCalculator.add("100");
-	// assert
-	        assertEquals(expected, actual);
-	    }
-	    @Test
-	    void StringCalculatorTest4() {
-	// arrange
-	        String expected = "5";
-	// actual
-	        String actual = stringCalculator.add("2,3");
-	// assert
-	        assertEquals(expected, actual);
-	    }
-	    
-	    @Test
-	    void StringCalculatorTest5() {
-	    	// arrange
-	        String expected = "101";
-	// actual
-	        String actual = stringCalculator.add("1,100");
-	// assert
-	        assertEquals(expected, actual);
-	    }
-	    @Test
-	    void StringCalculatorTest6() {
-	    	// arrange
-	        String expected = "15.6";
-	// actual
-	        String actual = stringCalculator.add("7.5,8.1");
-	// assert
-	        assertEquals(expected, actual);
-	    }
-	    void StringCalculatorTest7() {
-	    }
-	    void StringCalculatorTest8() {
-	    }
-	    void StringCalculatorTest9() {
-	    }
-	    void StringCalculatorTest10() {
-	    }
-	    void StringCalculatorTest11() {
-	    }
-	    void StringCalculatorTest12() {
-	    }
+	void StringCalculatorTest() {
+		// arrange
+		String expected = "7";
+		// actual
+		String actual = stringCalculator.add("7");
+		// assert
+		assertEquals(expected, actual);
 	}
 
+	@Test
+	/// TC 0
+	void StringCalculatorTest2() {
+		// arrange
+		String expected = "0";
+		// actual
+		String actual = stringCalculator.add("0");
+		// assert
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void StringCalculatorTest3() {
+		// arrange
+		String expected = "100";
+		// actual
+		String actual = stringCalculator.add("100");
+		// assert
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void StringCalculatorTest4() {
+		// arrange
+		String expected = "5";
+		// actual
+		String actual = stringCalculator.add("2,3");
+		// assert
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void StringCalculatorTest5() {
+		// arrange
+		String expected = "101";
+		// actual
+		String actual = stringCalculator.add("1,100");
+		// assert
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void StringCalculatorTest6() {
+		// arrange
+		String expected = "15.6";
+		// actual
+		String actual = stringCalculator.add("7.5,8.1");
+		// assert
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void StringCalculatorTest7() {
+		// arrange
+		String expected = "-65";
+		// actual
+		String actual = stringCalculator.add("-65");
+		// assert
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void StringCalculatorTest8() {
+		// arrange
+		String expected = "-61";
+		// actual
+		String actual = stringCalculator.add("-65,4");
+		// assert
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void StringCalculatorTest9() {
+		String expected = "-89";
+
+		String actual = stringCalculator.add("-65,-24");
+
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	void StringCalculatorTest10() {
+		String expected = "0";
+
+		String actual = stringCalculator.add("");
+
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	void StringCalculatorTest11() {
+
+		String expected = "27";
+
+		String actual = stringCalculator.add("21,6");
+
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	void StringCalculatorTest12() {
+
+		String expected = "43";
+
+		String actual = stringCalculator.add("41,2");
+
+		assertEquals(expected, actual);
+
+	}
+
+}
 
